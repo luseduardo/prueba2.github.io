d81855662275d8d645d97fa066d5988eb16b446b
luseduardo <1g.lopez.o.luis.eduardo@gmail.com>
2022-03-22 13:36:36 -0600

    prueba2

---
+++ ejercicio/ej1/ej1_resources/js/ardoraUnirCFG.js
@@ -0,0 +1,33 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=4; attempts=0; attemptsMax=3;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#004080";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=2; messageOk="Felicidades"; messageTime="Tiempo"; messageError="Error"; messageErrorG="Error"; messageAttempts="Intentos"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="ZWox"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[["TWFuemFuYQ==", "Um9qbw=="],["UGVyYQ==", "dmVyZGU="],["TWFy", "QXp1bA=="],["Q1Iv", "Um9uYWxkbw=="]];
+var c=[[7,4],[4,5],[3,4],[3,7]];
+var con1=["Manzana","Pera","Mar","CR/"];
+var con2=["Rojo","verde","Azul","Ronaldo"];
+var sel1=""; join1=[]; join2=[];
